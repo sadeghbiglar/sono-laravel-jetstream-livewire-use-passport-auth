eabd22b703b3dbd334ee0399d5ca1e1eaa69fdad
sadeghbiglar <sbmail555@gmail.com>
2024-11-09 18:42:24 +0330

    add bale_api code to archive with txt file for verify with social network

--- app/Http/Controllers/OtpVerificationController.php
+++ app/Http/Controllers/OtpVerificationController.php
@@ -12,33 +12,34 @@ class OtpVerificationController extends Controller
         return view('auth.otp');
     }
     public function sendOtp(Request $request)
-{
-    $mobile = $request->input('mobile');
-
-    // ارسال درخواست به API پیامک
-    $response = Http::post('https://console.melipayamak.com/api/send/otp/7b4745d0c47c422ca62d27edb7426f38', [
-        'to' => $mobile,
-    ]);
-
-   /*  // ارسال درخواست به API  اشتراکی پیامک
-     $response = Http::post('https://console.melipayamak.com/api/send/shared/7b4745d0c47c422ca62d27edb7426f38', [
-        'to' => $mobile,
-        'bodyId'=>524,
-        
-    ]);*/
+    {
+        $mobile = $request->input('mobile');
     
-    if ($response->successful()) {
-        $otpCode = $response->json()['code'];
-
-       //  ذخیره شماره موبایل و کد OTP در Session برای تأیید بعدی
-        session(['mobile' => $mobile, 'otp_code' => $otpCode]);
-      
-       
-        return redirect()->route('verify-otp')->with('status', 'کد OTP ارسال شد.');
-    } else {
-        return back()->withErrors('مشکلی در ارسال کد OTP رخ داد.');
+        // ارسال درخواست به API پیامک
+        $response = Http::post('https://console.melipayamak.com/api/send/otp/7b4745d0c47c422ca62d27edb7426f38', [
+            'to' => $mobile,
+        ]);
+    
+       /*  // ارسال درخواست به API  اشتراکی پیامک
+         $response = Http::post('https://console.melipayamak.com/api/send/shared/7b4745d0c47c422ca62d27edb7426f38', [
+            'to' => $mobile,
+            'bodyId'=>524,
+            
+        ]);*/
+        
+        if ($response->successful()) {
+            $otpCode = $response->json()['code'];
+    
+           //  ذخیره شماره موبایل و کد OTP در Session برای تأیید بعدی
+            session(['mobile' => $mobile, 'otp_code' => $otpCode]);
+          
+           
+            return redirect()->route('verify-otp')->with('status', 'کد OTP ارسال شد.');
+        } else {
+            return back()->withErrors('مشکلی در ارسال کد OTP رخ داد.');
+        }
     }
-}
+    
 public function verify(Request $request)
 {
     $request->validate([
